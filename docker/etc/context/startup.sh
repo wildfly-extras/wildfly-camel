0579c04e858b64ccb149eb876e9d4e1f5f72b6b6
Thomas Diesler <thomas.diesler@jboss.com>
2014-12-10 21:02:50 +0100

    [resolves #151] Remove hard coded domain controller password

--- docker/etc/context/startup.sh
+++ docker/etc/context/startup.sh
@@ -13,16 +13,18 @@ do
 done
 
 # Create default app server users
-if [[ ! -z "${WILDFLY_ADMIN_USER}" ]] && [[ ! -z "${WILDFLY_ADMIN_PASSWORD}" ]]
+if [[ ! -z "${WILDFLY_MANAGEMENT_USER}" ]] && [[ ! -z "${WILDFLY_MANAGEMENT_PASSWORD}" ]]
 then
-  ${JBOSS_HOME}/bin/add-user.sh --silent -e -u ${WILDFLY_ADMIN_USER} -p ${WILDFLY_ADMIN_PASSWORD}
-  unset ${WILDFLY_ADMIN_USER} ${WILDFLY_ADMIN_PASSWORD}
+  ${JBOSS_HOME}/bin/add-user.sh --silent -e -u ${WILDFLY_MANAGEMENT_USER} -p ${WILDFLY_MANAGEMENT_PASSWORD}
+  sed -i "s/@WILDFLY_MANAGEMENT_USER@/${WILDFLY_MANAGEMENT_USER}/" ${JBOSS_HOME}/domain/configuration/host-camel-slave.xml
+  sed -i "s/@WILDFLY_MANAGEMENT_PASSWORD@/`echo ${WILDFLY_MANAGEMENT_PASSWORD} | base64`/" ${JBOSS_HOME}/domain/configuration/host-camel-slave.xml
+  unset ${WILDFLY_MANAGEMENT_USER} ${WILDFLY_MANAGEMENT_PASSWORD}
 fi
 
-if [[ ! -z "${HAWTIO_USER}" ]] && [[ ! -z "${HAWTIO_PASSWORD}" ]]
+if [[ ! -z "${WILDFLY_APPLICATION_USER}" ]] && [[ ! -z "${WILDFLY_APPLICATION_PASSWORD}" ]]
 then
-  ${JBOSS_HOME}/bin/add-user.sh --silent -e -a -u ${HAWTIO_USER} -p ${HAWTIO_PASSWORD}
-  unset ${HAWTIO_USER} ${HAWTIO_PASSWORD}
+  ${JBOSS_HOME}/bin/add-user.sh --silent -e -a -u ${WILDFLY_APPLICATION_USER} -p ${WILDFLY_APPLICATION_PASSWORD}
+  unset ${WILDFLY_APPLICATION_USER} ${WILDFLY_APPLICATION_PASSWORD}
 fi
 
 # Run startup script for chosen operating mode
