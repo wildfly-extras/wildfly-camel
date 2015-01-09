7175c5190f9d3d460e0e1c35e44b04b375740a27
Thomas Diesler <thomas.diesler@jboss.com>
2015-01-09 10:31:26 +0100

    [resolves #24] Provide camel-hl7 integration

---
+++ itests/standalone/src/test/java/org/wildfly/camel/test/hl7/HL7IntegrationTest.java
@@ -0,0 +1,82 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.hl7;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.dataformat.HL7DataFormat;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelContextFactory;
+
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.Parser;
+
+@RunWith(Arquillian.class)
+public class HL7IntegrationTest {
+
+    @ArquillianResource
+    CamelContextFactory contextFactory;
+    
+    @Deployment
+    public static JavaArchive deployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "hl7-tests");
+        return archive;
+    }
+
+    @Test
+    public void testMarshalUnmarshal() throws Exception {
+        
+        final String msg = "MSH|^~\\&|MYSENDER|MYRECEIVER|MYAPPLICATION||200612211200||QRY^A19|1234|P|2.4\r";
+        final HL7DataFormat format = new HL7DataFormat();
+        
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                .marshal(format)
+                .unmarshal(format)
+                .to("mock:result");
+            }
+        });
+        camelctx.start();
+        
+        @SuppressWarnings("resource")
+        HapiContext context = new DefaultHapiContext();
+        Parser p = context.getGenericParser();
+        Message hapimsg = p.parse(msg);
+        
+        ProducerTemplate producer = camelctx.createProducerTemplate();
+        Message result = (Message) producer.requestBody("direct:start", hapimsg);
+        Assert.assertEquals(hapimsg.toString(), result.toString());
+    }
+}
