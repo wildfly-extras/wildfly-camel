7b8dd9a9d789c220b570a4b0100eb355d35e7c3e
Thomas Diesler <thomas.diesler@jboss.com>
2015-01-23 16:21:41 +0100

    [resolves #239] Make ConfigurationAdmin accessible through JMX

--- subsystem/src/main/java/org/wildfly/extension/camel/service/CamelBootstrapService.java
+++ subsystem/src/main/java/org/wildfly/extension/camel/service/CamelBootstrapService.java
@@ -24,16 +24,30 @@ import static org.wildfly.extension.camel.CamelLogger.LOGGER;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Dictionary;
+import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 import org.jboss.as.controller.ServiceVerificationHandler;
+import org.jboss.gravia.Constants;
 import org.jboss.gravia.repository.DefaultRepositoryXMLReader;
 import org.jboss.gravia.repository.Repository;
 import org.jboss.gravia.repository.RepositoryReader;
 import org.jboss.gravia.resolver.Environment;
 import org.jboss.gravia.resource.Resource;
+import org.jboss.gravia.resource.spi.AttachableSupport;
+import org.jboss.gravia.runtime.Runtime;
+import org.jboss.gravia.runtime.spi.AbstractModule;
+import org.jboss.gravia.runtime.spi.ClassLoaderEntriesProvider;
+import org.jboss.gravia.runtime.spi.ManifestHeadersProvider;
+import org.jboss.gravia.utils.IllegalStateAssertion;
 import org.jboss.modules.Module;
 import org.jboss.modules.ModuleClassLoader;
+import org.jboss.modules.ModuleIdentifier;
+import org.jboss.modules.ModuleLoader;
 import org.jboss.msc.service.AbstractService;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceController;
@@ -55,12 +69,14 @@ public final class CamelBootstrapService extends AbstractService<Void> {
 
     private final InjectedValue<Environment> injectedEnvironment = new InjectedValue<Environment>();
     private final InjectedValue<Repository> injectedRepository = new InjectedValue<Repository>();
+    private final InjectedValue<Runtime> injectedRuntime = new InjectedValue<Runtime>();
 
     public static ServiceController<Void> addService(ServiceTarget serviceTarget, ServiceVerificationHandler verificationHandler) {
         CamelBootstrapService service = new CamelBootstrapService();
         ServiceBuilder<Void> builder = serviceTarget.addService(CamelConstants.CAMEL_SUBSYSTEM_SERVICE_NAME, service);
         builder.addDependency(GraviaConstants.ENVIRONMENT_SERVICE_NAME, Environment.class, service.injectedEnvironment);
         builder.addDependency(GraviaConstants.REPOSITORY_SERVICE_NAME, Repository.class, service.injectedRepository);
+        builder.addDependency(GraviaConstants.RUNTIME_SERVICE_NAME, Runtime.class, service.injectedRuntime);
         builder.addListener(verificationHandler);
         return builder.install();
     }
@@ -75,8 +91,41 @@ public final class CamelBootstrapService extends AbstractService<Void> {
 
         // Install camel features to the repository
         installRepositoryContent(startContext);
+        
+        // Install and start third party stuff as gravia modules
+        activateThirdPartyModules();
     }
 
+    private void activateThirdPartyModules() throws StartException {
+        installSystemModule(ModuleIdentifier.create("org.jboss.gravia.jmx"), null);
+    }
+
+    private void installSystemModule(ModuleIdentifier moduleId, String match) throws StartException {
+        try {
+            ModuleLoader moduleLoader = Module.getCallerModuleLoader();
+            ModuleClassLoader classLoader = moduleLoader.loadModule(moduleId).getClassLoader();
+            Enumeration<URL> urls = classLoader.getResources(JarFile.MANIFEST_NAME);
+            IllegalStateAssertion.assertTrue(urls.hasMoreElements(), "Cannot find manifest for: " + moduleId);
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (match == null || url.getFile().contains(match)) {
+                    Manifest manifest = new Manifest(url.openStream());
+                    Dictionary<String, String> headers = new ManifestHeadersProvider(manifest).getHeaders();
+                    headers.put(Constants.GRAVIA_ENABLED, "true");
+                    AttachableSupport context = new AttachableSupport();
+                    ClassLoaderEntriesProvider entriesProvider = new ClassLoaderEntriesProvider(classLoader);
+                    context.putAttachment(AbstractModule.MODULE_ENTRIES_PROVIDER_KEY, entriesProvider);
+                    Runtime runtime = injectedRuntime.getValue();
+                    runtime.installModule(classLoader, null, headers, context).start();
+                }
+            }
+        } catch (RuntimeException rte) {
+            throw rte;
+        } catch (Exception ex) {
+            throw new StartException(ex);
+        }
+    }
+    
     private void installRepositoryContent(StartContext startContext) throws StartException {
 
         Repository repository = injectedRepository.getValue();
