12364951d2799f13af645db0f8da5e92bfeeabb9
Thomas Diesler <thomas.diesler@jboss.com>
2015-02-24 14:43:42 +0100

    [resolves #245] Provide camel-file support

---
+++ itests/standalone/src/test/java/org/wildfly/camel/test/file/FileIntegrationTest.java
@@ -0,0 +1,71 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.file;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class FileIntegrationTest {
+
+    @Deployment
+    public static JavaArchive createdeployment() throws IOException {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-file-tests");
+        return archive;
+    }
+
+    @Test
+    public void testFileEndpoint() throws Exception {
+        final String datadir = System.getProperty("jboss.server.data.dir");
+        Assert.assertNotNull("Directory name not nul", datadir);
+
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start").transform(body().prepend("Hello ")).
+                to("file:" + datadir + "?fileName=camel-file.txt");
+            }
+        });
+        camelctx.start();
+        ProducerTemplate producer = camelctx.createProducerTemplate();
+        String result = producer.requestBody("direct:start", "Kermit", String.class);
+        Assert.assertEquals("Hello Kermit", result);
+
+        BufferedReader br = new BufferedReader(new FileReader(Paths.get(datadir, "camel-file.txt").toFile()));
+        Assert.assertEquals("Hello Kermit", br.readLine());
+        br.close();
+    }
+}
