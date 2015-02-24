c10ce3fefbf237268a648346535f1219d7240cf2
Hiram Chirino <hiram@hiramchirino.com>
2015-02-24 11:51:59 -0500

    Fixes #328: Support disabling the camel subsystem entirely per deployment

---
+++ itests/standalone/src/test/java/org/wildfly/camel/test/NoCamelTest.java
@@ -0,0 +1,54 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.ResolveEndpointFailedException;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Verify that a deployment with a META-INF/jboss-all.xml file
+ * can disable camel from being added to your deployment.
+ */
+@RunWith(Arquillian.class)
+public class NoCamelTest {
+
+    @Deployment
+    public static JavaArchive deployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "no-mqtt-tests");
+        archive.addAsResource("jboss-all-no-camel.xml", "META-INF/jboss-all.xml");
+        return archive;
+    }
+
+    @Test(expected = NoClassDefFoundError.class)
+    public void testCamelDoesNotLoad() throws Exception {
+        new DefaultCamelContext();
+    }
+
+}
