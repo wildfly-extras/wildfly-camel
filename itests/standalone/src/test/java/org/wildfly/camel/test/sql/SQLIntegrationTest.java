e3c048a16ba3da4c5d35d75045aaf831473a6f24
Thomas Diesler <thomas.diesler@jboss.com>
2015-02-25 10:00:03 +0100

    [resolves #37] Provide camel-sql integration

---
+++ itests/standalone/src/test/java/org/wildfly/camel/test/sql/SQLIntegrationTest.java
@@ -0,0 +1,68 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.sql;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.PollingConsumer;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class SQLIntegrationTest {
+
+    @Deployment
+    public static JavaArchive createdeployment() throws IOException {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-sql-tests");
+        return archive;
+    }
+
+    @Test
+    public void testFileEndpoint() throws Exception {
+
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("sql:select name from information_schema.users?dataSource=java:jboss/datasources/ExampleDS")
+                .to("direct:end");
+            }
+        });
+        camelctx.start();
+
+        PollingConsumer pollingConsumer = camelctx.getEndpoint("direct:end").createPollingConsumer();
+        pollingConsumer.start();
+
+        String result = (String) pollingConsumer.receive().getIn().getBody(Map.class).get("NAME");
+        Assert.assertEquals("SA", result);
+
+        camelctx.stop();
+    }
+}
