e4005f10216eef5389cb4589e899f28457a7358a
Thomas Diesler <thomas.diesler@jboss.com>
2015-03-19 15:49:04 +0100

    [resolves #444] Add support for camel-crypto data format

---
+++ itests/standalone/src/test/java/org/wildfly/camel/test/crypto/CryptoIntegrationTest.java
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.crypto;
+
+import javax.crypto.KeyGenerator;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.crypto.CryptoDataFormat;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.camel.test.bindy.model.Customer;
+
+@RunWith(Arquillian.class)
+public class CryptoIntegrationTest {
+
+    @Deployment
+    public static JavaArchive deployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "crypto-tests");
+        archive.addPackage(Customer.class.getPackage());
+        return archive;
+    }
+
+    @Test
+    public void testMarshalUnmarshall() throws Exception {
+
+        final KeyGenerator generator = KeyGenerator.getInstance("DES");
+        final CryptoDataFormat cryptoFormat = new CryptoDataFormat("DES", generator.generateKey());
+
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                .marshal(cryptoFormat)
+                .unmarshal(cryptoFormat);
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            String result = producer.requestBody("direct:start", "password", String.class);
+            Assert.assertEquals("password", result.trim());
+        } finally {
+            //camelctx.stop();
+        }
+    }
+
+}
