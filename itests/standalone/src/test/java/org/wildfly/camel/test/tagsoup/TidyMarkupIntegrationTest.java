e143f9989b35adacc9144e1500be8f0bcef10a6f
Thomas Diesler <thomas.diesler@jboss.com>
2015-03-25 13:23:29 +0100

    [resolves #456] Add support for TidyMarkup data format

---
+++ itests/standalone/src/test/java/org/wildfly/camel/test/tagsoup/TidyMarkupIntegrationTest.java
@@ -0,0 +1,67 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.tagsoup;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class TidyMarkupIntegrationTest {
+
+    @Deployment
+    public static JavaArchive deployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "tagsoup-tests");
+        return archive;
+    }
+
+    @Test
+    public void testUnmarshal() throws Exception {
+
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                .unmarshal().tidyMarkup();
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            String result = producer.requestBody("direct:start", "<body><a href=foo></body>", String.class).trim();
+            Assert.assertTrue("Starts with html: " + result, result.startsWith("<html xmlns:html=\"http://www.w3.org/1999/xhtml\">"));
+            Assert.assertTrue("Contains end link: " + result, result.contains("</a>"));
+            Assert.assertTrue("Ends with html: " + result, result.endsWith("</html>"));
+        } finally {
+            camelctx.stop();
+        }
+    }
+}
