858f1fa1ea36ec1bf70e8d0b5db9384539944f79
Thomas Diesler <thomas.diesler@jboss.com>
2015-03-26 14:07:49 +0100

    [resolves #394] Add support for custom type converters

---
+++ itests/standalone/src/test/java/org/wildfly/camel/test/converter/CustomConverter.java
@@ -0,0 +1,46 @@
+/*
+ * #%L
+ * Wildfly Camel :: Example :: Camel CDI
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.converter;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.camel.Converter;
+import org.wildfly.camel.test.core.subA.Customer;
+
+// [FIXME CAMEL-8556] AnnotationTypeConverterLoader treats package as class
+
+@Converter
+public class CustomConverter {
+
+    @Converter
+    public Customer toCustomer(Map<String, String> map) {
+        return new Customer(map.get("firstName"), map.get("lastName"));
+    }
+
+    @Converter
+    public Map<String, String> toMap(Customer customer) {
+        Map<String, String> result = new LinkedHashMap<String, String>();
+        result.put("firstName", customer.getFirstName());
+        result.put("lastName", customer.getLastName());
+        return Collections.unmodifiableMap(result);
+    }
+}
