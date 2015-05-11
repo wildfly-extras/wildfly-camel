97e48eaa6dc022ab6bec26d1a4c01b428d591134
Thomas Diesler <thomas.diesler@jboss.com>
2015-05-11 14:59:04 +0200

    [resolves #397] Add support for parameter binding using annotioans

---
+++ itests/standalone/src/test/java/org/wildfly/camel/test/core/subA/OrderBean.java
@@ -0,0 +1,31 @@
+/*
+ * #%L
+ * Wildfly Camel :: Example :: Camel CDI
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.core.subA;
+
+import org.apache.camel.Body;
+import org.apache.camel.Header;
+
+
+public class OrderBean {
+
+    public String orderStatus(@Header("customerId") Integer customerId, @Body Integer orderId) {
+        return "Order " + orderId + " from customer " + customerId;
+    }
+}
