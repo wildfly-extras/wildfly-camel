37540a29d71c732581b2797baaddd676c06a7d42
Thomas Diesler <thomas.diesler@jboss.com>
2015-05-30 15:51:42 +0200

    [relates #626] Create test user / roles programmatically

--- itests/standalone/src/main/java/org/wildfly/camel/test/hawtio/HawtIOIntegrationTest.java
+++ itests/standalone/src/main/java/org/wildfly/camel/test/hawtio/HawtIOIntegrationTest.java
@@ -20,6 +20,9 @@
 
 package org.wildfly.camel.test.hawtio;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,37 +32,16 @@ import java.net.URLConnection;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.as.arquillian.api.ServerSetup;
-import org.jboss.as.arquillian.api.ServerSetupTask;
-import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.camel.test.common.UserManagement;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(Arquillian.class)
-@ServerSetup(HawtIOIntegrationTest.ManagementUserSetupTask.class)
 public class HawtIOIntegrationTest {
 
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "mgmnt-pa$$wrd1";
-
-    static class ManagementUserSetupTask implements ServerSetupTask {
-
-        @Override
-        public void setup(ManagementClient managementClient, String containerId) throws Exception {
-            UserManagement.managementUser().username(USERNAME).password(PASSWORD).create();
-        }
-
-        @Override
-        public void tearDown(final ManagementClient managementClient, String containerId) throws Exception {
-            // Do nothing
-        }
-    }
 
     @Deployment
     public static JavaArchive deployment() {
