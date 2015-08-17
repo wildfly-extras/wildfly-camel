ebf4a35be5364a8322ac28d011e92a7a5081e725
James Netherton <jamesnetherton@gmail.com>
2015-08-17 13:21:28 +0100

    [resolves #747] Add test coverage for Kafka consumer and producer

--- itests/standalone/basic/src/main/java/org/wildfly/camel/test/kafka/KafkaIntegrationTest.java
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/kafka/KafkaIntegrationTest.java
@@ -20,56 +20,95 @@
 
 package org.wildfly.camel.test.kafka;
 
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wildfly.camel.test.zookeeper.EmbeddedZookeeperServer;
 
 @RunWith(Arquillian.class)
 public class KafkaIntegrationTest {
 
-    static EmbeddedZookeeperServer server;
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaIntegrationTest.class);
+    private static final String TEST_TOPIC_NAME = "test";
+    private static final int KAFKA_PORT = 9092;
+    private static final int ZOOKEEPER_PORT = 2181;
+    private static EmbeddedZookeeperServer zkServer;
+    private static EmbeddedKafkaCluster kafkaCluster;
 
     @Deployment
     public static JavaArchive deployment() {
-        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "kafka-integration-tests");
-        archive.addClasses(EmbeddedZookeeperServer.class);
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "kafka-integration-tests.jar");
+        archive.addClasses(EmbeddedZookeeperServer.class, EmbeddedKafkaCluster.class, KafkaConstants.class);
         return archive;
     }
 
-    @Before
-    public void before() throws Exception {
-        server = new EmbeddedZookeeperServer().startup(1, TimeUnit.SECONDS);
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        List<Integer> kafkaPorts = new ArrayList<>();
+        kafkaPorts.add(KAFKA_PORT);
+
+        zkServer = new EmbeddedZookeeperServer(ZOOKEEPER_PORT, true);
+        kafkaCluster = new EmbeddedKafkaCluster(zkServer.getConnection(), new Properties(), kafkaPorts);
+
+        zkServer.startup(1, TimeUnit.SECONDS);
+        kafkaCluster.startup();
     }
 
-    @After
-    public void after() throws Exception {
-        if (server != null) {
-            server.shutdown();
+    @AfterClass
+    public static void afterClass() throws Exception {
+        if (kafkaCluster != null) {
+            kafkaCluster.shutdown();
+        }
+
+        if (zkServer != null) {
+            zkServer.shutdown();
         }
     }
 
     @Test
-    public void testKafkaEndpoint() throws Exception {
-
+    public void testKafkaProducer() throws Exception {
         String zkhost = "&zookeeperHost=localhost";
-        String zkport = "&zookeeperPort=" + server.getServerPort();
+        String zkport = "&zookeeperPort=" + ZOOKEEPER_PORT;
         String serializer = "&serializerClass=kafka.serializer.StringEncoder";
-        final String epuri = "kafka:localhost:9092?topic=test&groupId=group1" + zkhost + zkport + serializer;
+        final String epuri = "kafka:localhost:" + KAFKA_PORT + "?topic=" + TEST_TOPIC_NAME + "&groupId=group1" + zkhost + zkport + serializer;
 
         CamelContext camelctx = new DefaultCamelContext();
+        ConsumerConnector kafkaMessageConsumer = null;
+
         camelctx.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
@@ -79,15 +118,118 @@ public class KafkaIntegrationTest {
 
         camelctx.start();
         try {
-            Endpoint endpoint = camelctx.getEndpoint(epuri);
-            Assert.assertEquals(endpoint.getClass().getName(), "org.apache.camel.component.kafka.KafkaEndpoint");
+            CountDownLatch latch = new CountDownLatch(5);
+            kafkaMessageConsumer = createKafkaMessageConsumer(latch);
 
-            // [TODO] Send and verify an actual message
-            //ProducerTemplate producer = camelctx.createProducerTemplate();
-            //String result = producer.requestBody("direct:start", "Kermit", String.class);
+            Map<String, Object> headerMap = new HashMap<>();
+            headerMap.put(KafkaConstants.PARTITION_KEY, "1");
 
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            for (int i = 0; i < 5; i++) {
+                producer.sendBodyAndHeaders("direct:start", "Camel Kafka test message " + i, headerMap);
+            }
+
+            boolean result = latch.await(5, TimeUnit.SECONDS);
+            Assert.assertTrue("Expected " + 5 + " messages. Not received: " + latch.getCount(), result);
         } finally {
             camelctx.stop();
+            if (kafkaMessageConsumer != null) {
+                kafkaMessageConsumer.shutdown();
+            }
+        }
+    }
+
+    @Test
+    public void testKafkaConsumer() throws Exception {
+        String zkhost = "&zookeeperHost=localhost";
+        String zkport = "&zookeeperPort=" + ZOOKEEPER_PORT;
+        final String epuri = "kafka:localhost:" + KAFKA_PORT + "?topic=" + TEST_TOPIC_NAME + "&groupId=group1&autoOffsetReset=smallest" + zkhost + zkport;
+
+        final CountDownLatch latch = new CountDownLatch(5);
+
+        Producer producer = createKafkaMessageProducer();
+        CamelContext camelctx = new DefaultCamelContext();
+
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from(epuri).process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        LOG.info("Consumed message {}", exchange.getIn().getBody());
+                        latch.countDown();
+                    }
+                });
+            }
+        });
+
+        camelctx.start();
+        try {
+            for (int i = 0; i < 5; i++) {
+                KeyedMessage<String, String> data = new KeyedMessage<>(TEST_TOPIC_NAME, "1", "Camel Kafka test message " + i);
+                producer.send(data);
+            }
+
+            boolean result = latch.await(10, TimeUnit.SECONDS);
+            Assert.assertTrue("Expected to consume 5 messages but consumed " + latch.getCount(), result);
+        } finally {
+            producer.close();
+            camelctx.stop();
+        }
+    }
+
+    private ConsumerConnector createKafkaMessageConsumer(CountDownLatch messagesLatch) {
+        Properties consumerProps = new Properties();
+        consumerProps.put("zookeeper.connect", "localhost:" + ZOOKEEPER_PORT);
+        consumerProps.put("group.id", "group1");
+        consumerProps.put("zookeeper.session.timeout.ms", "6000");
+        consumerProps.put("zookeeper.connectiontimeout.ms", "12000");
+        consumerProps.put("zookeeper.sync.time.ms", "200");
+        consumerProps.put("auto.commit.interval.ms", "1000");
+        consumerProps.put("auto.offset.reset", "smallest");
+
+        ConsumerConnector kafkaConsumer = Consumer.createJavaConsumerConnector(new ConsumerConfig(consumerProps));
+
+        Map<String, Integer> topicCountMap = new HashMap<>();
+        topicCountMap.put(TEST_TOPIC_NAME, 5);
+
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = kafkaConsumer.createMessageStreams(topicCountMap);
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (KafkaStream<byte[], byte[]> stream : consumerMap.get(TEST_TOPIC_NAME)) {
+            executor.submit(new KakfaTopicConsumer(stream, messagesLatch));
+        }
+
+        return kafkaConsumer;
+    }
+
+    private Producer<Object, Object> createKafkaMessageProducer() {
+        Properties properties = new Properties();
+        properties.put("metadata.broker.list", "localhost:" + KAFKA_PORT);
+        properties.put("serializer.class", "kafka.serializer.StringEncoder");
+        properties.put("partitioner.class", kafka.producer.DefaultPartitioner.class.getName());
+        properties.put("request.required.acks", "1");
+
+        ProducerConfig config = new ProducerConfig(properties);
+        return new Producer<>(config);
+    }
+
+    private static class KakfaTopicConsumer implements Runnable {
+        private KafkaStream<byte[], byte[]> stream;
+        private CountDownLatch latch;
+
+        public KakfaTopicConsumer(KafkaStream<byte[], byte[]> stream, CountDownLatch latch) {
+            this.stream = stream;
+            this.latch = latch;
+        }
+
+        @Override
+        public void run() {
+            ConsumerIterator<byte[], byte[]> it = stream.iterator();
+            while (it.hasNext()) {
+                String msg = new String(it.next().message());
+                LOG.info("Received message: {}", msg);
+                latch.countDown();
+            }
         }
     }
 }
