b070f1e7bbb7f7288aa9dc029e4b734236f3d8d2
James Netherton <jamesnetherton@gmail.com>
2015-09-14 21:21:40 +0100

    [resolves #860] Add test coverage for markRollbackOnly DSL command

---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/jpa/TransactionalJpaIntegrationTest.java
@@ -0,0 +1,135 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2015 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.jpa;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+
+import org.apache.camel.CamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.camel.test.jpa.subA.Account;
+import org.wildfly.camel.test.jpa.subA.JpaRouteBuilder;
+import org.wildfly.extension.camel.CamelContextRegistry;
+
+@RunWith(Arquillian.class)
+public class TransactionalJpaIntegrationTest {
+
+    @ArquillianResource
+    CamelContextRegistry contextRegistry;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Resource(mappedName = "java:jboss/UserTransaction")
+    private UserTransaction utx;
+
+    @Deployment
+    public static JavaArchive deployment() {
+        JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-jpa-tests.jar");
+        archive.addPackage(JpaRouteBuilder.class.getPackage());
+        archive.addAsResource("jpa/persistence.xml", "META-INF/persistence.xml");
+        archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        return archive;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // Insert some test data
+        utx.begin();
+        em.joinTransaction();
+        em.persist(new Account(1, 750));
+        em.persist(new Account(2, 300));
+        utx.commit();
+        em.clear();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Clean up test data
+        utx.begin();
+        em.joinTransaction();
+        em.createQuery("delete from Account").executeUpdate();
+        utx.commit();
+    }
+
+    @Test
+    public void testJpaTransactionalRoute() throws Exception {
+
+        CamelContext camelctx = contextRegistry.getCamelContext("jpa-cdi-context");
+
+        // Update account id 2 with 250
+        Map<String, Integer> params = new HashMap<>();
+        params.put("sourceAccountId", 1);
+        params.put("targetAccountId", 2);
+        params.put("amount", 250);
+
+        camelctx.createProducerTemplate().sendBody("direct:start", params);
+
+        // Account A should have been decremented by 250, Account B should have been credited 250
+        Account accountA = em.getReference(Account.class, 1);
+        Account accountB = em.getReference(Account.class, 2);
+
+        Assert.assertEquals(500, accountA.getBalance());
+        Assert.assertEquals(550, accountB.getBalance());
+    }
+
+    @Test
+    public void testJpaTransactionalRouteRollback() throws Exception {
+
+        CamelContext camelctx = contextRegistry.getCamelContext("jpa-cdi-context");
+
+        // Attempt to update account 2 with 1500 which is greater than max allowed amount
+        Map<String, Integer> params = new HashMap<>();
+        params.put("sourceAccountId", 1);
+        params.put("targetAccountId", 2);
+        params.put("amount", 550);
+
+        camelctx.createProducerTemplate().sendBody("direct:start", params);
+
+        // Transaction should have been rolled back and accounts left unmodified
+        Account accountA = em.getReference(Account.class, 1);
+        Account accountB = em.getReference(Account.class, 2);
+
+        Assert.assertEquals(750, accountA.getBalance());
+        Assert.assertEquals(300, accountB.getBalance());
+
+        // Transaction should have been dead lettered
+        String deadLetterDir = System.getProperty("jboss.server.data.dir") + "/deadletter/";
+        Assert.assertTrue(new File(Paths.get(deadLetterDir, "deadLetters.xml").toUri()).exists());
+    }
+}
---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/jpa/subA/JpaRouteBuilder.java
@@ -0,0 +1,96 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2015 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.jpa.subA;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.Startup;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.ContextName;
+import org.apache.camel.component.jpa.JpaComponent;
+
+@Startup
+@ContextName("jpa-cdi-context")
+public class JpaRouteBuilder extends RouteBuilder {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Inject
+    WildflyJtaTransactionManager transactionManager;
+
+    @Override
+    public void configure() throws Exception {
+
+        // Configure JPA component
+        JpaComponent jpaComponent = new JpaComponent();
+        jpaComponent.setEntityManagerFactory(em.getEntityManagerFactory());
+        jpaComponent.setTransactionManager(transactionManager);
+        getContext().addComponent("jpa", jpaComponent);
+
+        onException(IllegalArgumentException.class)
+            .maximumRedeliveries(1)
+            .handled(true)
+            .convertBodyTo(String.class)
+            .to("file:{{jboss.server.data.dir}}/deadletter?fileName=deadLetters.xml")
+            .markRollbackOnly();
+
+        from("direct:start").to("log:end?showAll=true")
+            .transacted()
+            .setHeader("targetAccountId", simple("${body[targetAccountId]}"))
+            .setHeader("amount", simple("${body[amount]}"))
+
+        // Take amount from the source account and decrement balance
+        .to("sql:update account set balance = balance - :#amount where id = :#sourceAccountId?dataSource=wildFlyExampleDS")
+        .choice()
+            .when(simple("${header.amount} > 500"))
+                .log("Amount is too large! Rolling back transaction")
+                .throwException(new IllegalArgumentException("Amount too large"))
+            .otherwise()
+                .to("direct:txsmall");
+
+        from("direct:txsmall")
+        .to("sql:select balance from account where id = :#targetAccountId?dataSource=wildFlyExampleDS")
+        .process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                List<Map<String, Object>> result = (List<Map<String, Object>>) exchange.getIn().getBody();
+                int id = exchange.getIn().getHeader("targetAccountId", Integer.class);
+                int amount = exchange.getIn().getHeader("amount", Integer.class);
+                int balance = (int) result.get(0).get("BALANCE");
+
+                // Update target account with new balance
+                Account account = em.find(Account.class, id);
+
+                account.setBalance(balance + amount);
+                exchange.getOut().setBody(account);
+            }
+        })
+        .to("jpa://org.wildfly.test.jpa.subA.Account");
+    }
+}
