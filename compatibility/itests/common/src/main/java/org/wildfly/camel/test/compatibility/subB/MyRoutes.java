6a186a36c49b3cc69aa765e48a7bddb0ce1c62bb
Thomas Diesler <thomas.diesler@jboss.com>
2015-11-24 13:59:19 +0100

    [resolves #986] Evaluate compatibility for CDI based components

---
+++ compatibility/itests/common/src/main/java/org/wildfly/camel/test/compatibility/subB/MyRoutes.java
@@ -0,0 +1,33 @@
+package org.wildfly.camel.test.compatibility.subB;
+
+import javax.inject.Inject;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.ContextName;
+import org.apache.camel.cdi.Uri;
+
+/**
+ * Configures all our Camel routes, components, endpoints and beans
+ */
+@ContextName
+public class MyRoutes extends RouteBuilder {
+
+    @Inject
+    @Uri("timer:foo?period=500")
+    private Endpoint inputEndpoint;
+
+    @Inject
+    @Uri("log:output")
+    private Endpoint resultEndpoint;
+
+    @Override
+    public void configure() throws Exception {
+        // you can configure the route rule with Java DSL here
+
+        from(inputEndpoint)
+            .to("bean:counterBean")
+            .to(resultEndpoint);
+    }
+
+}
