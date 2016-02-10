8d79085592eaf252f64c85f22d64788791f00afe
James Netherton <jamesnetherton@gmail.com>
2016-02-10 11:33:56 +0000

    [resolves #1050] Add support for PropertyPlaceholderConfigurer

---
+++ itests/standalone/smoke/src/main/java/org/wildfly/camel/test/spring/SpringPropertyPlaceholderTest.java
@@ -0,0 +1,63 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2016 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.spring;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.camel.test.spring.subC.PropertyValueBean;
+import org.wildfly.extension.camel.CamelAware;
+import org.wildfly.extension.camel.CamelContextRegistry;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class SpringPropertyPlaceholderTest {
+
+    @ArquillianResource
+    CamelContextRegistry contextRegistry;
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-spring-tests");
+        archive.addAsResource("spring/property-placeholder-context.xml", "spring-camel-context.xml");
+        archive.addAsResource("classloading/test.properties", "test.properties");
+        archive.addClass(PropertyValueBean.class);
+        return archive;
+    }
+
+    @Test
+    public void testSpringPropertyPlaceholder() throws Exception {
+        CamelContext camelctx = contextRegistry.getCamelContext("prop-placeholder-context");
+        Assert.assertNotNull("Expected camel context to not be null", camelctx);
+
+        ProducerTemplate producerTemplate = camelctx.createProducerTemplate();
+        String result = producerTemplate.requestBody("direct:start", null, String.class);
+
+        Assert.assertEquals("TestValue", result);
+    }
+
+}
