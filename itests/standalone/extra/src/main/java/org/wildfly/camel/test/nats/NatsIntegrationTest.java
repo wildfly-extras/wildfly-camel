f8b6906e12a8d4d97a5ef53b3c0771f4d6dceb01
Andrea Cosentino <ancosen@gmail.com>
2016-05-10 14:19:05 +0200

    [resolves #1194] Add support for Camel-NATS

---
+++ itests/standalone/extra/src/main/java/org/wildfly/camel/test/nats/NatsIntegrationTest.java
@@ -0,0 +1,122 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2016 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.nats;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.component.nats.NatsConsumer;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nats.Connection;
+import org.wildfly.extension.camel.CamelAware;
+
+/**
+ * Tests camel-nats integration.
+ * Requires a locally running GNATSD instance: https://github.com/nats-io/gnatsd/releases
+ */
+@CamelAware
+@RunWith(Arquillian.class)
+public class NatsIntegrationTest {
+
+    private static ProcessBuilder pb;
+    private static Process p;
+    private static final String GNATSD_PATH = "GNATSD_PATH";
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "camel-nats-tests.jar");
+    }
+
+    @Test
+    public void testNatsComponent() throws Exception {
+        CamelContext camelctx = new DefaultCamelContext();
+        Endpoint endpoint = camelctx.getEndpoint("nats://localhost:4222?topic=test");
+        Assert.assertNotNull(endpoint);
+        Assert.assertEquals(endpoint.getClass().getName(), "org.apache.camel.component.nats.NatsEndpoint");
+    }
+
+    @Test
+    public void testNatsRoutes() throws Exception {
+        String gnatsdPath = System.getenv().get(GNATSD_PATH);
+        Assume.assumeTrue(gnatsdPath != null && new File(gnatsdPath).exists());
+
+        pb = new ProcessBuilder(gnatsdPath, "-DV", "-a", "127.0.0.1", "-l", "target/gnatsd.log");
+        try {
+            p = pb.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        CamelContext camelctx = new DefaultCamelContext();
+        try {
+            camelctx.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("nats://localhost:4222?topic=test").id("nats-route")
+                    .to("mock:result");
+                }
+            });
+
+            MockEndpoint to = camelctx.getEndpoint("mock:result", MockEndpoint.class);
+            to.expectedBodiesReceived("message");
+            to.expectedMessageCount(1);
+
+            camelctx.start();
+
+            // Make sure the consumer has subscribed to the topic before sending messages
+            NatsConsumer consumer = (NatsConsumer) camelctx.getRoute("nats-route").getConsumer();
+            int count = 0;
+            while(!consumer.isSubscribed()) {
+                Thread.sleep(500);
+                count += 1;
+                if (count > 10) {
+                    throw new IllegalStateException("Gave up waiting for nats consumer to subscribe to topic");
+                }
+            }
+
+            Properties opts = new Properties();
+            opts.put("servers", "nats://localhost:4222");
+
+            Connection conn = Connection.connect(opts);
+            conn.publish("test", "message");
+
+            to.assertIsSatisfied(10000);
+
+        } finally {
+            camelctx.stop();
+            if (p != null) {
+                p.destroy();
+            }
+        }
+    }
+}
