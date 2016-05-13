15207609612c7e30cbbf8d3dd0a27937ada86cce
Andrea Cosentino <ancosen@gmail.com>
2016-05-13 14:37:18 +0200

    [resolved #1199] Add support for Camel-DNS

---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/dns/DnsIntegrationTest.java
@@ -0,0 +1,83 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.dns;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.dns.DnsConstants;
+import org.apache.camel.component.dns.DnsType;
+import org.apache.camel.http.common.HttpOperationFailedException;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelAware;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Type;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class DnsIntegrationTest {
+
+    @Deployment
+    public static JavaArchive createdeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "camel-dns-tests");
+    }
+
+    @Test
+    public void testDnsComponent() throws Exception {
+        CamelContext camelctx = new DefaultCamelContext();
+        Endpoint endpoint = camelctx.getEndpoint("dns:lookup");
+        Assert.assertNotNull(endpoint);
+        Assert.assertEquals(endpoint.getClass().getName(), "org.apache.camel.component.dns.DnsEndpoint");
+    }
+
+    @Test
+    public void testDnsProducer() throws Exception {
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                .to("dns:lookup");
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            Record[] record = producer.requestBodyAndHeader("direct:start", null, DnsConstants.DNS_NAME, "redhat.com", Record[].class);
+
+            Assert.assertEquals("redhat.com.", record[0].getName().toString());
+            Assert.assertEquals(Type.A, record[0].getType());
+        } finally {
+            camelctx.stop();
+        }
+    }
+}
