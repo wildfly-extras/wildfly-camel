f22f11c8a33bbbbca2f1e8c92c507a25c4a5d6c4
James Netherton <jamesnetherton@gmail.com>
2016-08-30 10:38:33 +0100

    [resolves #1315] Add support for camel-base64

---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/base64/Base64IntegrationTest.java
@@ -0,0 +1,87 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2016 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.base64;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelAware;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class Base64IntegrationTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-test.jar");
+        return archive;
+    }
+
+    @Test
+    public void testBase64Marshal() throws Exception {
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                 from("direct:start").marshal().base64();
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate template = camelctx.createProducerTemplate();
+            String result = template.requestBody("direct:start", "Hello Kermit!", String.class);
+
+            Assert.assertEquals("SGVsbG8gS2VybWl0IQ==", result.trim());
+        } finally {
+            camelctx.stop();
+        }
+    }
+
+    @Test
+    public void testBase64Unmarshal() throws Exception {
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start").unmarshal().base64();
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate template = camelctx.createProducerTemplate();
+            String result = template.requestBody("direct:start", "SGVsbG8gS2VybWl0IQo=", String.class);
+
+            Assert.assertEquals("Hello Kermit!", result.trim());
+        } finally {
+            camelctx.stop();
+        }
+    }
+}
