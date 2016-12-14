e6fc2bd72a140a8b34f68ea171347631c35f5128
Thomas Diesler <thomas.diesler@jboss.com>
2016-12-14 14:20:50 +0100

    [resolves #1493] Add support for language jsonpath

---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/jsonpath/JsonpathIntegrationTest.java
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.jsonpath;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.camel.test.common.types.Customer;
+import org.wildfly.extension.camel.CamelAware;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class JsonpathIntegrationTest {
+
+    @Deployment
+    public static JavaArchive deployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "jsonpath-tests");
+        archive.addClasses(Customer.class);
+        return archive;
+    }
+
+    @Test
+    public void testUnmarshalJson() throws Exception {
+
+        final String type = "Customer";
+        
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                .choice()
+                .when().jsonpath("$." + type + "[?(@.firstName == 'John')]")
+                .transform(simple("Doe"));
+            }
+        });
+
+        String input = "{'" + type + "':{'firstName':'John'}}";
+
+        camelctx.start();
+        try {
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            String result = producer.requestBody("direct:start", input, String.class);
+            Assert.assertEquals("Doe", result);
+        } finally {
+            camelctx.stop();
+        }
+    }
+}
