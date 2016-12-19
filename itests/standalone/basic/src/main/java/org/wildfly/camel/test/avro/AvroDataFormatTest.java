4a7a9139f8da26e4d342cd1d744e2120c128e345
Thomas Diesler <thomas.diesler@jboss.com>
2016-12-19 14:58:02 +0100

    [resolves #1480] Add support for dataformat avro

---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/avro/AvroDataFormatTest.java
@@ -0,0 +1,85 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.avro;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.avro.AvroDataFormat;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.spi.DataFormat;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelAware;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class AvroDataFormatTest {
+
+    @Deployment
+    public static JavaArchive createdeployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-avro-tests");
+        archive.addAsResource("avro/user.avsc");
+        return archive;
+    }
+
+    @Test
+    public void testMarshalUnmarshal() throws Exception {
+
+        DataFormat avro = new AvroDataFormat(getSchema());
+        GenericRecord input = new GenericData.Record(getSchema());
+        input.put("name", "Kermit");
+        
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start").marshal(avro).unmarshal(avro);
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            GenericRecord result = producer.requestBody("direct:start", input, GenericRecord.class);
+            Assert.assertEquals("Kermit", result.get("name").toString());
+        } finally {
+            camelctx.stop();
+        }
+    }
+
+    private Schema getSchema() throws IOException {
+        try (InputStream input = getClass().getResourceAsStream("/avro/user.avsc")) {
+            return new Schema.Parser().parse(input);
+        }
+    }
+}
