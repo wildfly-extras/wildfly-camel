7ff4af9e3d7a71ffde759b21ff5e4c97857c3973
James Netherton <jamesnetherton@gmail.com>
2017-04-20 18:08:24 +0100

    [resolves #1421] Add support for component coap

---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/coap/CoapIntegrationTest.java
@@ -0,0 +1,77 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2017 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.coap;
+
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.eclipse.californium.core.network.config.NetworkConfig;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.gravia.resource.ManifestBuilder;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.camel.test.common.utils.AvailablePortFinder;
+import org.wildfly.extension.camel.CamelAware;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class CoapIntegrationTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "camel-coap-tests.jar")
+            .addClass(AvailablePortFinder.class)
+            .setManifest(() -> {
+                ManifestBuilder builder = new ManifestBuilder();
+                builder.addManifestHeader("Dependencies", "org.eclipse.californium");
+                return builder.openStream();
+            });
+    }
+
+    @Test
+    public void testCoapComponent() throws Exception {
+        NetworkConfig.createStandardWithoutFile();
+
+        String coapConsumerUri = String.format("coap://localhost:%d/hello", AvailablePortFinder.getNextAvailable());
+
+        DefaultCamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                fromF(coapConsumerUri)
+                .convertBodyTo(String.class)
+                .setBody(simple("Hello ${body}"));
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate template = camelctx.createProducerTemplate();
+            String result = template.requestBody(coapConsumerUri + "?coapMethod=POST", "Kermit", String.class);
+            Assert.assertEquals("Hello Kermit", result);
+        } finally {
+            camelctx.stop();
+        }
+    }
+}
