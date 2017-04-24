437d4c15bdb27cd7d6019fb7058a09aca0f8956c
James Netherton <jamesnetherton@gmail.com>
2017-04-24 14:31:59 +0100

    [resolves #1756] Add test coverage for camel REST DSL with Spring

---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/rest/RestDslSpringIntegrationTest.java
@@ -0,0 +1,54 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2017 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.rest;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.camel.test.common.http.HttpRequest;
+import org.wildfly.extension.camel.CamelAware;
+import org.wildfly.extension.camel.CamelContextRegistry;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class RestDslSpringIntegrationTest {
+
+    @ArquillianResource
+    CamelContextRegistry contextRegistry;
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "camel-rest-spring-tests.jar")
+            .addAsResource("rest/rest-camel-context.xml", "camel-context.xml")
+            .addClass(HttpRequest.class);
+    }
+
+    @Test
+    public void testComponent() throws Exception {
+        Assert.assertNotNull("Camel context is null", contextRegistry.getCamelContext("rest-context"));
+        String body = HttpRequest.get("http://127.0.0.1:8080/say/hello").getResponse().getBody();
+        Assert.assertEquals("Hello Kermit", body);
+    }
+}
