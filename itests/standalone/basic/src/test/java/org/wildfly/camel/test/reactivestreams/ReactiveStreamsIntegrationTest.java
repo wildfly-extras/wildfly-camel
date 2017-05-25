d87a7795fbb6dd6dddfa298cf1331d23cbba8425
James Netherton <jamesnetherton@gmail.com>
2017-05-25 12:08:07 +0100

    [resolves #1847] Add support for component reactive-streams

---
+++ itests/standalone/basic/src/test/java/org/wildfly/camel/test/reactivestreams/ReactiveStreamsIntegrationTest.java
@@ -0,0 +1,79 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2017 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.reactivestreams;
+
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.component.reactive.streams.api.CamelReactiveStreams;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.wildfly.extension.camel.CamelAware;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class ReactiveStreamsIntegrationTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "camel-reactive-streams-tests.jar");
+    }
+
+    @Test
+    public void testStreamPublishSubscribe() throws Exception {
+        DefaultCamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                .to("reactive-streams:pub");
+
+                from("reactive-streams:sub")
+                .to("mock:result");
+            }
+        });
+
+        MockEndpoint mockEndpoint = camelctx.getEndpoint("mock:result", MockEndpoint.class);
+        mockEndpoint.expectedBodiesReceivedInAnyOrder(1, 2, 3, 4, 5);
+
+        camelctx.start();
+        try {
+            Subscriber<Integer> sub = CamelReactiveStreams.get(camelctx).streamSubscriber("sub", Integer.class);
+            Publisher<Integer> pub = CamelReactiveStreams.get(camelctx).fromStream("pub", Integer.class);
+            pub.subscribe(sub);
+
+            ProducerTemplate template = camelctx.createProducerTemplate();
+            for (int i = 1; i <= 5; i++) {
+                template.sendBody("direct:start", i);
+            }
+
+            mockEndpoint.assertIsSatisfied();
+        } finally {
+            camelctx.stop();
+        }
+    }
+}
