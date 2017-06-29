9348c69c0b3b23d6ce8276867f3f07f52bdcdb20
James Netherton <jamesnetherton@gmail.com>
2017-06-29 07:26:01 +0100

    [resolves #1909] JMS MDB example shuts down with inflight exchanges

--- examples/camel-jms-mdb/src/main/java/org/wildfly/camel/examples/jms/JmsRouteBuilder.java
+++ examples/camel-jms-mdb/src/main/java/org/wildfly/camel/examples/jms/JmsRouteBuilder.java
@@ -53,7 +53,7 @@ public class JmsRouteBuilder extends RouteBuilder {
          * This route uses the timer component to generate a message which is sent to
          * the JMS OrdersQueue
          */
-        from("timer:produceJMSMessage?period=5000&delay=-1")
+        from("timer:produceJMSMessage?period=5s&delay=0&fixedRate=true")
         .process(new Processor() {
             @Override
             public void process(Exchange exchange) throws Exception {
