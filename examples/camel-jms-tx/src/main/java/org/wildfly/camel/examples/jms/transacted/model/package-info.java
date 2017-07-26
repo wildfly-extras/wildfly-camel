bc6c7737e9115c84c03d6fa5783673b816aa1afc
James Netherton <jamesnetherton@gmail.com>
2017-07-26 09:09:25 +0100

    [resolves #1931] Migrate examples to wildfly-camel-examples project

--- examples/camel-jms-tx/src/main/java/org/wildfly/camel/examples/jms/transacted/model/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://org/wildfly/camel/examples/jms/transacted/model/Order", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.wildfly.camel.examples.jms.transacted.model;
