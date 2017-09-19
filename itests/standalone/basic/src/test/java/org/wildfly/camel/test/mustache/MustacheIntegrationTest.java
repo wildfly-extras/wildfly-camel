2c04d6125a1ee4ef8884c50b24aa5aa1cd1f2b6f
James Netherton <jamesnetherton@gmail.com>
2017-09-19 08:01:23 +0100

    [resolves #2018] Add support for component mustache

---
+++ itests/standalone/basic/src/test/java/org/wildfly/camel/test/mustache/MustacheIntegrationTest.java
@@ -0,0 +1,70 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2017 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.mustache;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelAware;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class MustacheIntegrationTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "camel-mustache-tests.jar")
+            .addAsResource("mustache/hello.mustache", "template.mustache");
+    }
+
+    @Test
+    public void testMustacheComponent() throws Exception {
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                .to("mustache:classpath:template.mustache");
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate template = camelctx.createProducerTemplate();
+            Exchange response = template.request("direct:start", exchange -> {
+                exchange.getIn().setHeader("greeting", "Hello");
+                exchange.getIn().setHeader("name", "Kermit");
+            });
+
+            Assert.assertEquals("Hello Kermit!", response.getOut().getBody(String.class));
+        } finally {
+            camelctx.stop();
+        }
+    }
+}
