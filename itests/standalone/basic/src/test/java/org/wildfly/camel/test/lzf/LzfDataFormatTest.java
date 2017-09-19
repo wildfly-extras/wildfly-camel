397c6d8c50881034e7c7335f1004e37e0f748ea0
Thomas Diesler <tdiesler@redhat.com>
2017-09-19 19:14:51 +0200

    [resolves #2044] Add support for dataformat lzf

---
+++ itests/standalone/basic/src/test/java/org/wildfly/camel/test/lzf/LzfDataFormatTest.java
@@ -0,0 +1,104 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2014 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.lzf;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.IOConverter;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelAware;
+
+import com.ning.compress.lzf.LZFInputStream;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class LzfDataFormatTest {
+
+    private static final String TEXT = "Hamlet by William Shakespeare\n"
+            + "To be, or not to be: that is the question:\n"
+            + "Whether 'tis nobler in the mind to suffer\n"
+            + "The slings and arrows of outrageous fortune,\n"
+            + "Or to take arms against a sea of troubles,\n"
+            + "And by opposing end them? To die: to sleep;";
+
+    @Deployment
+    public static JavaArchive deployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "lzf-dataformat-tests");
+        return archive;
+    }
+
+    @Test
+    public void testMarshal() throws Exception {
+
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:textToLzf")
+                .marshal().lzf();
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            byte[] output = (byte[]) producer.requestBody("direct:textToLzf", TEXT.getBytes("UTF-8"));
+            InputStream stream = new LZFInputStream(new ByteArrayInputStream(output));
+            Assert.assertEquals(TEXT, IOConverter.toString(stream, null));
+        } finally {
+            camelctx.stop();
+        }
+    }
+
+    @Test
+    public void testUnmarshal() throws Exception {
+
+        CamelContext camelctx = new DefaultCamelContext();
+        camelctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:unmarshalTextToLzf")
+                .marshal().lzf()
+                .unmarshal().lzf();
+            }
+        });
+
+        camelctx.start();
+        try {
+            ProducerTemplate producer = camelctx.createProducerTemplate();
+            String result = producer.requestBody("direct:unmarshalTextToLzf", TEXT.getBytes("UTF-8"), String.class);
+            Assert.assertEquals(TEXT, result);
+        } finally {
+            camelctx.stop();
+        }
+    }
+}
