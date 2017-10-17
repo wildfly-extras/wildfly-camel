c66e5f1ce80d29d76e6d22c501bea5b449adfbf8
James Netherton <jamesnetherton@gmail.com>
2017-10-17 13:58:50 +0100

    [resolves #1983] Add support for component asterisk

---
+++ itests/standalone/basic/src/test/java/org/wildfly/camel/test/asterisk/AsteriskIntegrationTest.java
@@ -0,0 +1,49 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2017 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.camel.test.asterisk;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.asterisk.AsteriskEndpoint;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.camel.CamelAware;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class AsteriskIntegrationTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "camel-asterisk-tests.jar");
+    }
+
+    @Test
+    public void testAsteriskComponentLoads() throws Exception {
+        CamelContext camelctx = new DefaultCamelContext();
+        AsteriskEndpoint endpoint = camelctx.getEndpoint("asterisk://myVoIP?hostname=localhost&username=foo&password=bar", AsteriskEndpoint.class);
+        Assert.assertNotNull("AsteriskEndpoint is null", endpoint);
+    }
+}
