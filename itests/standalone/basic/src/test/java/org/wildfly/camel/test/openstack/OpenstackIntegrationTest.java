e804326edf7551b84134c99347ce15435196cbba
Thomas Diesler <tdiesler@redhat.com>
2017-11-03 17:41:40 +0100

    [resolves #1843] Add support for component openstack-neutron

--- itests/standalone/basic/src/test/java/org/wildfly/camel/test/openstack/OpenstackIntegrationTest.java
+++ itests/standalone/basic/src/test/java/org/wildfly/camel/test/openstack/OpenstackIntegrationTest.java
@@ -18,6 +18,7 @@ package org.wildfly.camel.test.openstack;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -28,6 +29,7 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
@@ -35,6 +37,9 @@ import org.apache.camel.Message;
 import org.apache.camel.Producer;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.openstack.common.OpenstackConstants;
+import org.apache.camel.component.openstack.neutron.NeutronConstants;
+import org.apache.camel.component.openstack.neutron.NeutronEndpoint;
+import org.apache.camel.component.openstack.neutron.producer.NetworkProducer;
 import org.apache.camel.component.openstack.nova.NovaEndpoint;
 import org.apache.camel.component.openstack.nova.producer.KeypairProducer;
 import org.apache.camel.component.openstack.swift.SwiftConstants;
@@ -55,16 +60,24 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.objenesis.Objenesis;
+import org.openstack4j.api.Builders;
 import org.openstack4j.api.OSClient;
 import org.openstack4j.api.compute.ComputeService;
 import org.openstack4j.api.compute.FlavorService;
 import org.openstack4j.api.compute.KeypairService;
 import org.openstack4j.api.compute.ServerService;
+import org.openstack4j.api.networking.NetworkService;
+import org.openstack4j.api.networking.NetworkingService;
+import org.openstack4j.api.networking.PortService;
+import org.openstack4j.api.networking.RouterService;
+import org.openstack4j.api.networking.SubnetService;
 import org.openstack4j.api.storage.ObjectStorageContainerService;
 import org.openstack4j.api.storage.ObjectStorageObjectService;
 import org.openstack4j.api.storage.ObjectStorageService;
 import org.openstack4j.model.common.ActionResponse;
 import org.openstack4j.model.compute.Keypair;
+import org.openstack4j.model.network.Network;
+import org.openstack4j.model.network.NetworkType;
 import org.openstack4j.model.storage.object.options.CreateUpdateContainerOptions;
 import org.openstack4j.openstack.compute.domain.NovaKeypair;
 import org.wildfly.extension.camel.CamelAware;
@@ -76,16 +89,27 @@ public class OpenstackIntegrationTest {
     private static final String CONTAINER_NAME = "containerName";
     private static final String KEYPAIR_NAME = "keypairName";
 
+    // swift
     ObjectStorageService objectStorageService = Mockito.mock(ObjectStorageService.class);
     ObjectStorageContainerService containerService = Mockito.mock(ObjectStorageContainerService.class);
     ObjectStorageObjectService objectService = Mockito.mock(ObjectStorageObjectService.class);
-    
+
+    // nova
     ComputeService computeService = Mockito.mock(ComputeService.class);
-    FlavorService flavorService  = Mockito.mock(FlavorService.class);
+    FlavorService flavorService = Mockito.mock(FlavorService.class);
     ServerService serverService = Mockito.mock(ServerService.class);
     KeypairService keypairService = Mockito.mock(KeypairService.class);
     Keypair osTestKeypair = Mockito.mock(Keypair.class);
-    Keypair dummyKeypair = NovaKeypair.create(KEYPAIR_NAME, "string contains private key");
+    Keypair dummyKeypair = createKeypair();
+
+    // neutron
+    NetworkingService networkingService = Mockito.mock(NetworkingService.class);
+    PortService portService = Mockito.mock(PortService.class);
+    RouterService routerService = Mockito.mock(RouterService.class);
+    SubnetService subnetService = Mockito.mock(SubnetService.class);
+    NetworkService networkService = Mockito.mock(NetworkService.class);
+    Network testOSnetwork = Mockito.mock(Network.class);
+    Network dummyNetwork = createNetwork();
 
     OSClient.OSClientV3 client = Mockito.mock(OSClient.OSClientV3.class);
 
@@ -98,30 +122,52 @@ public class OpenstackIntegrationTest {
 
     @Before
     public void before() throws IOException {
-        when(client.objectStorage()).thenReturn(objectStorageService);
-        when(client.compute()).thenReturn(computeService);
-        
+
+        // swift
         when(objectStorageService.containers()).thenReturn(containerService);
         when(objectStorageService.objects()).thenReturn(objectService);
+        when(client.objectStorage()).thenReturn(objectStorageService);
 
+        // nova
         when(computeService.flavors()).thenReturn(flavorService);
         when(computeService.servers()).thenReturn(serverService);
         when(computeService.keypairs()).thenReturn(keypairService);
-        
+        when(client.compute()).thenReturn(computeService);
+
         when(keypairService.get(Matchers.anyString())).thenReturn(osTestKeypair);
         when(keypairService.create(Matchers.anyString(), Matchers.anyString())).thenReturn(osTestKeypair);
 
-        List<org.openstack4j.model.compute.Keypair> getAllList = new ArrayList<>();
-        getAllList.add(osTestKeypair);
-        getAllList.add(osTestKeypair);
-        doReturn(getAllList).when(keypairService).list();
+        List<Keypair> keypairList = new ArrayList<>();
+        keypairList.add(osTestKeypair);
+        keypairList.add(osTestKeypair);
+        doReturn(keypairList).when(keypairService).list();
 
         when(osTestKeypair.getName()).thenReturn(dummyKeypair.getName());
         when(osTestKeypair.getPublicKey()).thenReturn(dummyKeypair.getPublicKey());
+
+        // neutron
+        when(networkingService.port()).thenReturn(portService);
+        when(networkingService.router()).thenReturn(routerService);
+        when(networkingService.subnet()).thenReturn(subnetService);
+        when(networkingService.network()).thenReturn(networkService);
+        when(client.networking()).thenReturn(networkingService);
+
+        when(networkService.create(any(Network.class))).thenReturn(testOSnetwork);
+        when(networkService.get(anyString())).thenReturn(testOSnetwork);
+
+        List<Network> getAllList = new ArrayList<>();
+        getAllList.add(testOSnetwork);
+        getAllList.add(testOSnetwork);
+        doReturn(getAllList).when(networkService).list();
+
+        when(testOSnetwork.getName()).thenReturn(dummyNetwork.getName());
+        when(testOSnetwork.getTenantId()).thenReturn(dummyNetwork.getTenantId());
+        when(testOSnetwork.getNetworkType()).thenReturn(dummyNetwork.getNetworkType());
+        when(testOSnetwork.getId()).thenReturn(UUID.randomUUID().toString());
     }
 
     @Test
-    public void createTestWithoutOptionsMock() throws Exception {
+    public void createSwiftContainer() throws Exception {
 
         CamelContext camelContext = Mockito.mock(CamelContext.class);
         when(camelContext.getHeadersMapFactory()).thenReturn(new DefaultHeadersMapFactory());
@@ -160,14 +206,14 @@ public class OpenstackIntegrationTest {
 
         CamelContext camelContext = Mockito.mock(CamelContext.class);
         when(camelContext.getHeadersMapFactory()).thenReturn(new DefaultHeadersMapFactory());
-        
+
         Message msg = new DefaultMessage(camelContext);
         msg.setHeader(OpenstackConstants.OPERATION, OpenstackConstants.CREATE);
         msg.setHeader(OpenstackConstants.NAME, KEYPAIR_NAME);
 
         Exchange exchange = Mockito.mock(Exchange.class);
         when(exchange.getIn()).thenReturn(msg);
-        
+
         NovaEndpoint endpoint = Mockito.mock(NovaEndpoint.class);
         Producer producer = new KeypairProducer(endpoint, client);
         producer.process(exchange);
@@ -187,6 +233,31 @@ public class OpenstackIntegrationTest {
     }
 
     @Test
+    public void createNeutronNetwork() throws Exception {
+        CamelContext camelContext = Mockito.mock(CamelContext.class);
+        when(camelContext.getHeadersMapFactory()).thenReturn(new DefaultHeadersMapFactory());
+        
+        Message msg = new DefaultMessage(camelContext);
+        Exchange exchange = Mockito.mock(Exchange.class);
+        when(exchange.getIn()).thenReturn(msg);
+        
+        msg.setHeader(OpenstackConstants.OPERATION, OpenstackConstants.CREATE);
+        msg.setHeader(OpenstackConstants.NAME, dummyNetwork.getName());
+        msg.setHeader(NeutronConstants.NETWORK_TYPE, dummyNetwork.getNetworkType());
+        msg.setHeader(NeutronConstants.TENANT_ID, dummyNetwork.getTenantId());
+
+        NeutronEndpoint endpoint = Mockito.mock(NeutronEndpoint.class);
+        Producer producer = new NetworkProducer(endpoint, client);
+        producer.process(exchange);
+
+        ArgumentCaptor<Network> captor = ArgumentCaptor.forClass(Network.class);
+        verify(networkService).create(captor.capture());
+
+        assertEqualsNetwork(dummyNetwork, captor.getValue());
+        assertNotNull(msg.getBody(Network.class).getId());
+    }
+
+    @Test
     public void testEndpoints() throws Exception {
 
         CamelContext camelctx = new DefaultCamelContext();
@@ -194,6 +265,7 @@ public class OpenstackIntegrationTest {
             public void configure() throws Exception {
                 from("direct:start").to("openstack-swift:localhost");
                 from("direct:start").to("openstack-nova:localhost");
+                from("direct:start").to("openstack-neutron:localhost");
             }
         });
 
@@ -201,6 +273,23 @@ public class OpenstackIntegrationTest {
         Assert.assertNotNull("SwiftEndpoint not null", swiftEndpoint);
 
         NovaEndpoint novaEndpoint = camelctx.getEndpoint("openstack-nova:localhost", NovaEndpoint.class);
-        Assert.assertNotNull("SwiftEndpoint not null", novaEndpoint);
+        Assert.assertNotNull("NovaEndpoint not null", novaEndpoint);
+
+        NeutronEndpoint neutronEndpoint = camelctx.getEndpoint("openstack-neutron:localhost", NeutronEndpoint.class);
+        Assert.assertNotNull("NeutronEndpoint not null", neutronEndpoint);
+    }
+
+    private NovaKeypair createKeypair() {
+        return NovaKeypair.create(KEYPAIR_NAME, "string contains private key");
+    }
+
+    private Network createNetwork() {
+        return Builders.network().name("name").tenantId("tenantID").networkType(NetworkType.LOCAL).build();
+    }
+
+    private void assertEqualsNetwork(Network old, Network newNetwork) {
+        assertEquals(old.getName(), newNetwork.getName());
+        assertEquals(old.getTenantId(), newNetwork.getTenantId());
+        assertEquals(old.getNetworkType(), newNetwork.getNetworkType());
     }
 }
