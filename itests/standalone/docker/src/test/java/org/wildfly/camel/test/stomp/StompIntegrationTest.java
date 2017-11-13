6fc129b8567f0bd8aa4a7cf0937fba0e8c19b5b4
Peter Palaga <ppalaga@redhat.com>
2017-11-13 18:09:00 +0100

    [resolves #2271] Stomp integration fails on EAP-7.1

--- itests/standalone/docker/src/test/java/org/wildfly/camel/test/stomp/StompIntegrationTest.java
+++ itests/standalone/docker/src/test/java/org/wildfly/camel/test/stomp/StompIntegrationTest.java
@@ -33,55 +33,61 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.arquillian.cube.CubeController;
+import org.arquillian.cube.docker.impl.requirement.RequiresDocker;
+import org.arquillian.cube.requirement.ArquillianConditionalRunner;
 import org.fusesource.stomp.client.BlockingConnection;
 import org.fusesource.stomp.client.Stomp;
 import org.fusesource.stomp.codec.StompFrame;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.as.arquillian.api.ServerSetup;
-import org.jboss.as.arquillian.api.ServerSetupTask;
-import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.camel.test.common.utils.JMSUtils;
+import org.wildfly.camel.test.common.utils.TestUtils;
 import org.wildfly.extension.camel.CamelAware;
 
 /**
  * @author <a href="https://github.com/ppalaga">Peter Palaga</a>
  */
 @CamelAware
-@RunWith(Arquillian.class)
-@ServerSetup({ StompIntegrationTest.JmsQueueSetup.class })
+@RequiresDocker
+@RunWith(ArquillianConditionalRunner.class)
 public class StompIntegrationTest {
 
-    private static final int STOMP_PORT = 61613;
+    private static final String CONTAINER_NAME = "amq";
+    private static final int STOMP_PORT = 42653;
     static final String QUEUE_NAME = "stompq";
     static final String QUEUE_JNDI_NAME = "java:/" + QUEUE_NAME;
-    static final String ACTIVEMQ_USER = "user1";
-    static final String ACTIVEMQ_PASSWORD = "appl-pa$$wrd1";
 
     @ArquillianResource
     InitialContext initialctx;
 
-    static class JmsQueueSetup implements ServerSetupTask {
+    @ArquillianResource
+    private CubeController cubeController;
+    private String amqUrl;
 
-        @Override
-        public void setup(ManagementClient managementClient, String containerId) throws Exception {
-            JMSUtils.createJmsQueue(QUEUE_NAME, QUEUE_JNDI_NAME, managementClient.getControllerClient());
-        }
 
-        @Override
-        public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
-            JMSUtils.removeJmsQueue(QUEUE_NAME, managementClient.getControllerClient());
-        }
+    @Before
+    public void setUp() throws Exception {
+        cubeController.create(CONTAINER_NAME);
+        cubeController.start(CONTAINER_NAME);
+        amqUrl = "tcp://"+ TestUtils.getDockerHost() +":"+ STOMP_PORT;
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        cubeController.stop(CONTAINER_NAME);
+        cubeController.destroy(CONTAINER_NAME);
     }
 
     @Deployment
     public static JavaArchive createdeployment() {
-        return ShrinkWrap.create(JavaArchive.class, "camel-stomp-tests");
+        return ShrinkWrap.create(JavaArchive.class, "camel-stomp-tests")
+                .addClass(TestUtils.class);
     }
 
     @Test
@@ -91,8 +97,7 @@ public class StompIntegrationTest {
         camelctx.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                fromF("stomp:jms.queue.%s?brokerURL=tcp://127.0.0.1:%d&login=%s&passcode=%s", QUEUE_NAME, STOMP_PORT,
-                        ACTIVEMQ_USER, ACTIVEMQ_PASSWORD) //
+                fromF("stomp:jms.queue.%s?brokerURL=%s", QUEUE_NAME, amqUrl) //
                                 .transform(body().method("utf8").prepend("Hello "))//
                                 .to("mock:result");
             }
@@ -104,9 +109,7 @@ public class StompIntegrationTest {
         mockEndpoint.setExpectedCount(1);
         mockEndpoint.expectedBodiesReceived("Hello Kermit");
 
-        Stomp stomp = new Stomp("tcp://127.0.0.1:" + STOMP_PORT);
-        stomp.setLogin(ACTIVEMQ_USER);
-        stomp.setPasscode(ACTIVEMQ_PASSWORD);
+        Stomp stomp = new Stomp(amqUrl);
         final BlockingConnection producerConnection = stomp.connectBlocking();
         try {
             StompFrame outFrame = new StompFrame(SEND);
