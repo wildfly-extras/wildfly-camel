eb69088d84bd6a4c238d6d6476ca7ffe3ed9c616
Thomas Diesler <tdiesler@redhat.com>
2020-07-03 12:32:33 +0200

    [resolves #2674] Intermittent failure of StompIntegrationTest

--- itests/standalone/basic/src/test/java/org/wildfly/camel/test/stomp/StompIntegrationTest.java
+++ itests/standalone/basic/src/test/java/org/wildfly/camel/test/stomp/StompIntegrationTest.java
@@ -25,9 +25,11 @@ import static org.fusesource.stomp.client.Constants.DESTINATION;
 import static org.fusesource.stomp.client.Constants.MESSAGE_ID;
 import static org.fusesource.stomp.client.Constants.SEND;
 
+import java.util.Date;
 import java.util.UUID;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -41,9 +43,10 @@ import org.jboss.as.arquillian.api.ServerSetupTask;
 import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wildfly.camel.test.common.utils.AvailablePortFinder;
 import org.wildfly.camel.test.common.utils.UserManager;
 import org.wildfly.camel.test.common.utils.WildFlyCli;
 import org.wildfly.extension.camel.CamelAware;
@@ -51,21 +54,30 @@ import org.wildfly.extension.camel.CamelAware;
 @CamelAware
 @RunWith(Arquillian.class)
 @ServerSetup({StompIntegrationTest.StompConnectorSetupTask.class})
-@Ignore("[#2674] Intermittent failure of StompIntegrationTest")
 public class StompIntegrationTest {
 
-    private static final int STOMP_PORT = 61613;
-    private static final String BROKER_URL = "tcp://localhost:" + STOMP_PORT;
     private static final String QUEUE_NAME = "wfc";
     private static final String USERNAME = "stomp-user";
     private static final String PASSWORD = "stomp-password";
 
+    @Deployment
+    public static JavaArchive createDeployment() {
+        JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-stomp-tests.jar");
+        archive.addClasses(AvailablePortFinder.class);
+		return archive;
+    }
+
     static class StompConnectorSetupTask implements ServerSetupTask {
+    	
         @Override
         public void setup(ManagementClient managementClient, String containerId) throws Exception {
-            String cliScript = "/subsystem=messaging-activemq/server=default/acceptor=stomp-acceptor"
+            
+        	int stompPort = AvailablePortFinder.getNextAvailable(61613);
+        	AvailablePortFinder.storeServerData("stomp-port", stompPort);
+        	
+        	String cliScript = "/subsystem=messaging-activemq/server=default/acceptor=stomp-acceptor"
                 + ":add(factory-class=org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory,"
-                + "params={protocols=STOMP, port=" + STOMP_PORT + "})\n"
+                + "params={protocols=STOMP, port=" + stompPort + "})\n"
                 + "reload";
 
             new WildFlyCli().run(cliScript).assertSuccess();
@@ -78,8 +90,10 @@ public class StompIntegrationTest {
 
         @Override
         public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
-            String cliScript = "/subsystem=messaging-activemq/server=default/acceptor=stomp-acceptor:remove\n"
+            
+        	String cliScript = "/subsystem=messaging-activemq/server=default/acceptor=stomp-acceptor:remove\n"
                 + "reload";
+            
             new WildFlyCli().run(cliScript).assertSuccess();
 
             try (UserManager userManager = UserManager.forStandaloneApplicationRealm()) {
@@ -89,48 +103,52 @@ public class StompIntegrationTest {
         }
     }
 
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class, "camel-stomp-tests.jar");
-    }
-
     @Test
     public void testMessageConsumerRoute() throws Exception {
-        CamelContext camelctx = new DefaultCamelContext();
-        camelctx.addRoutes(new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                fromF("stomp:%s?login=%s&passcode=%s&brokerURL=%s", QUEUE_NAME, USERNAME, PASSWORD, BROKER_URL)
-                .transform(body().prepend("Hello "))
-                .to("mock:result");
-            }
-        });
+    	
+        final String brokerURL = "tcp://localhost:" + AvailablePortFinder.readServerData("stomp-port");
+        System.out.println("BrokerURL: " + brokerURL);
+        
+        try (CamelContext camelctx = new DefaultCamelContext()) {
+        	
+            camelctx.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    fromF("stomp:%s?login=%s&passcode=%s&brokerURL=%s", QUEUE_NAME, USERNAME, PASSWORD, brokerURL)
+                    .transform(body().prepend("Hello "))
+                    .to("mock:result");
+                }
+            });
 
-        camelctx.start();
+            camelctx.start();
 
-        MockEndpoint mockEndpoint = camelctx.getEndpoint("mock:result", MockEndpoint.class);
-        mockEndpoint.expectedBodiesReceived("Hello Kermit");
-        mockEndpoint.setResultWaitTime(30000);
+            MockEndpoint mockEndpoint = camelctx.getEndpoint("mock:result", MockEndpoint.class);
+            mockEndpoint.expectedMessageCount(1);
 
-        Stomp stomp = new Stomp(BROKER_URL);
-        stomp.setLogin(USERNAME);
-        stomp.setPasscode(PASSWORD);
+            Stomp stomp = new Stomp(brokerURL);
+            stomp.setLogin(USERNAME);
+            stomp.setPasscode(PASSWORD);
 
-        final BlockingConnection producerConnection = stomp.connectBlocking();
-        try {
-            StompFrame outFrame = new StompFrame(SEND);
-            outFrame.addHeader(DESTINATION, StompFrame.encodeHeader(QUEUE_NAME));
-            outFrame.addHeader(MESSAGE_ID, StompFrame
-                .encodeHeader("StompIntegrationTest.testMessageConsumerRoute" + UUID.randomUUID().toString()));
-            outFrame.content(utf8("Kermit"));
-            producerConnection.send(outFrame);
+            final BlockingConnection producerConnection = stomp.connectBlocking();
+            try {
+                StompFrame frame = new StompFrame(SEND);
+                frame.addHeader(DESTINATION, StompFrame.encodeHeader(QUEUE_NAME));
+                frame.addHeader(MESSAGE_ID, StompFrame.encodeHeader("StompIntegrationTest.testMessageConsumerRoute" + UUID.randomUUID().toString()));
+                frame.content(utf8("Kermit"));
+                
+                producerConnection.send(frame);
+                System.out.println(String.format("StompFrame sent: %s", new Date()));
 
-            mockEndpoint.assertIsSatisfied();
-        } finally {
-            if (producerConnection != null) {
+                mockEndpoint.assertIsSatisfied(30000);
+                System.out.println(String.format("Mock satisfied: %s", new Date()));
+                
+                Exchange exchange = mockEndpoint.getExchanges().get(0);
+                String body = exchange.getMessage().getBody(String.class);
+                Assert.assertEquals("Hello ascii: Kermit", body);
+                
+            } finally {
                 producerConnection.close();
             }
-            camelctx.close();
         }
     }
 }
